5838a2375340b0b753d8d3d8d23b9d0dd7f47257
Andrei Mandychev <dev@dappli.fr>
2024-11-02 23:01:36 +0100

    add cache to form repository

--- core/data/repository/src/main/kotlin/fr/dappli/portailfamilles/core/data/repository/FormRepositoryImpl.kt
+++ core/data/repository/src/main/kotlin/fr/dappli/portailfamilles/core/data/repository/FormRepositoryImpl.kt
@@ -7,19 +7,34 @@ import fr.dappli.portailfamilles.core.data.repository.mapper.FormMapper
 import fr.dappli.portailfamilles.core.data.repository.mapper.toDomainException
 import fr.dappli.portailfamilles.core.domain.irepository.FormRepository
 import fr.dappli.portailfamilles.core.domain.model.form.Form
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class FormRepositoryImpl @Inject constructor(
     private val formsDataSource: FormsDataSource,
     private val persistentDataSource: PersistentDataSource,
     private val mapper: FormMapper
 ): FormRepository {
 
-    override suspend fun getForms(): List<Form> {
+    private val mutex = Mutex()
+    private val inMemoryCache = mutableMapOf<String, List<Form>>()
+
+    override suspend fun getForms(forceRefresh: Boolean): List<Form> {
         return try {
             val userId = persistentDataSource.getString(PersistenceKey.USER_ID.name) ?:
                 throw InternalError("No user id to get forms")
-            mapper.map(formsDataSource.getForms(userId))
+
+            val cache = mutex.withLock { inMemoryCache[userId] }
+            if (forceRefresh.not() && cache != null) {
+                cache
+            } else {
+                mapper.map(formsDataSource.getForms(userId)).also {
+                    mutex.withLock { inMemoryCache[userId] = it }
+                }
+            }
         } catch (e: Throwable) {
             throw e.toDomainException()
         }
--- core/domain/irepository/src/main/kotlin/fr/dappli/portailfamilles/core/domain/irepository/FormRepository.kt
+++ core/domain/irepository/src/main/kotlin/fr/dappli/portailfamilles/core/domain/irepository/FormRepository.kt
@@ -3,5 +3,5 @@ package fr.dappli.portailfamilles.core.domain.irepository
 import fr.dappli.portailfamilles.core.domain.model.form.Form
 
 interface FormRepository {
-    suspend fun getForms(): List<Form>
+    suspend fun getForms(forceRefresh: Boolean = false): List<Form>
 }
