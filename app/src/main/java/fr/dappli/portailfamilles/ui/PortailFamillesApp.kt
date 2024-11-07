5a241e4776e3727eb0896959d69b018240bb2e0e
Andrei Mandychev <dev@dappli.fr>
2024-11-07 16:15:16 +0100

    PortailFamillesAppBar with navigationIcon

--- app/src/main/java/fr/dappli/portailfamilles/ui/PortailFamillesApp.kt
+++ app/src/main/java/fr/dappli/portailfamilles/ui/PortailFamillesApp.kt
@@ -6,23 +6,17 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import fr.dappli.portailfamilles.R
+import fr.dappli.portailfamilles.navigation.PortailFamillesAppBar
 import fr.dappli.portailfamilles.navigation.PortailFamillesNavBar
 import fr.dappli.portailfamilles.navigation.PortailFamillesNavHost
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PortailFamillesApp(
     appState: PortailFamillesAppState
@@ -30,21 +24,8 @@ fun PortailFamillesApp(
     Scaffold(
         containerColor = Color.Transparent,
         contentColor = MaterialTheme.colorScheme.onBackground,
-        contentWindowInsets = WindowInsets(0, 0, 0, 0),
         topBar = {
-
-            val stackEntry by appState.navController.currentBackStackEntryFlow.collectAsStateWithLifecycle(null)
-            println("andrei stackEntry=$stackEntry")
-
-            TopAppBar(
-                colors = topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                title = {
-                    Text(stringResource(R.string.app_name))
-                }
-            )
+            PortailFamillesAppBar(appState)
         },
         bottomBar = {
             val isUserAuthenticated by appState.isUserAuthenticated.collectAsStateWithLifecycle()
