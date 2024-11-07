82a46e791e52b25d0f2e4e7bbcc7cde5ddf6e511
Andrei Mandychev <dev@dappli.fr>
2024-11-07 17:45:56 +0100

    add WorkInProgress

--- feature/reservation/src/main/kotlin/fr/dappli/portailfamilles/feature/reservation/ui/ReservationSubCategoryScreen.kt
+++ feature/reservation/src/main/kotlin/fr/dappli/portailfamilles/feature/reservation/ui/ReservationSubCategoryScreen.kt
@@ -1,9 +1,9 @@
 package fr.dappli.portailfamilles.feature.reservation.ui
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import fr.dappli.portailfamilles.core.ui.component.WorkInProgress
 
 @Composable
 fun ReservationSubCategoryScreen() {
-    Text("ReservationSubCategoryScreen")
+    WorkInProgress()
 }
